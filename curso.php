7f2eeaeb02e672f102e0e762d7873a7b7731fa3f
Ju4nix159 <juanimelillo@gmail.com>
2024-09-04 23:24:37 -0300

    cambio en los nombres de la sbd
    se cambiaron los nombre en la base de datos

--- curso.php
+++ curso.php
@@ -1,18 +1,19 @@
 <?php 
     require_once 'sbd.php';
     include ("nav.php");
+    
     $id_curso = $_GET['id_curso'];
 
 
-    $sql_cursos = $con->prepare("SELECT * FROM cursos c JOIN niveles n ON c.id_complejidad = n.id_complejidad WHERE c.id = :id");
-    $sql_cursos->bindParam(':id', $id_curso);
+    $sql_cursos = $con->prepare("SELECT * FROM cursos c JOIN complejidad n ON c.id_complejidad = n.id_complejidad WHERE c.id_curso = :id_curso");
+    $sql_cursos->bindParam(':id_curso', $id_curso);
     $sql_cursos->execute();
     $curso = $sql_cursos->fetch(PDO::FETCH_ASSOC);
 
     
 
-    $sql_complejidad = $con->prepare("SELECT m.id AS modalidad_id, m.nombre AS modalidad_nombre FROM curso_modalidad cm JOIN modalidades m ON cm.id_modalidad = m.id WHERE cm.id_curso = :id");
-    $sql_complejidad->bindParam(':id', $id_curso);
+    $sql_complejidad = $con->prepare("SELECT m.id_modalidad AS modalidad_id, m.nombre_modalidad AS modalidad_nombre FROM curso_modalidad cm JOIN modalidades m ON cm.id_modalidad = m.id_modalidad WHERE cm.id_curso = :id_curso");
+    $sql_complejidad->bindParam(':id_curso', $id_curso);
     $sql_complejidad->execute();
     $modalidades = $sql_complejidad->fetchAll(PDO::FETCH_ASSOC);
 
@@ -30,7 +31,7 @@
     /* foreach ($modalidades as $key => $value) {
         echo "<p><strong>$key:</strong> $value</p>";
     } imprime todas los valores con clave valor de lo que devuelve una query  */
-     
+
    
 
     
@@ -43,12 +44,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title><?php echo $curso["nombre"]?></title>
+    <title><?php echo $curso["nombre_curso"]?></title>
 </head>
 <body>
 <div class="jumbotron jumbotron-fluid py-5">
         <div class="container">
-            <h1 class="display-4"><?php echo $curso["nombre"]?></h1>
+            <h1 class="display-4"><?php echo $curso["nombre_curso"]?></h1>
             <p class="lead">subtitulo o explicacion rapida del curso</p>
         </div>
     </div>
@@ -57,7 +58,7 @@
         <div class="row">
             <div class="col-md-8">
                 <h2>Descripción del Curso</h2>
-                <p><?php echo $curso['descripcion']?></p>
+                <p><?php echo $curso['descripcion_curso']?></p>
                 
                 <h3 class="mt-4">Objetivos del Curso</h3>
                 <ul>
@@ -74,7 +75,7 @@
                     </div>
                     <div class="card-body">
                         <p><strong>Duración:</strong> <?php echo $curso["duracion"]?></p>
-                        <p><strong>Nivel:</strong> <?php echo $curso["nombre_nivel"]?></p>
+                        <p><strong>Nivel:</strong> <?php echo $curso["nombre_complejidad"]?></p>
                         <p><strong>Modalidad:</strong> <?php echo $modalidad_nombres_str ?></p>
                         <p><strong>Certificación:</strong> Sí</p>
                         <a href="#" class="btn btn-primary btn-block">Inscribirse Ahora</a>
@@ -83,6 +84,7 @@
             </div>
         </div>
     </div>
+    <?php include("footer.php")?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="app.js"></script>
 </body>
--- cursos.php
+++ cursos.php
@@ -6,7 +6,7 @@ $page = isset($_POST['page']) ? (int)$_POST['page'] : 1;
 $items_per_page = 6;
 $offset = ($page - 1) * $items_per_page;
 
-$sql = $con->prepare("SELECT * FROM cursos WHERE nombre LIKE :search LIMIT :offset, :items_per_page");
+$sql = $con->prepare("SELECT * FROM cursos WHERE nombre_curso LIKE :search LIMIT :offset, :items_per_page");
 $sql->bindValue(':search', "%$search%", PDO::PARAM_STR);
 $sql->bindValue(':offset', $offset, PDO::PARAM_INT);
 $sql->bindValue(':items_per_page', $items_per_page, PDO::PARAM_INT);
@@ -14,7 +14,7 @@ $sql->execute();
 $cursos = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 // Obteniendo el total de cursos para calcular la paginación
-$total_sql = $con->prepare("SELECT COUNT(*) FROM cursos WHERE nombre LIKE :search");
+$total_sql = $con->prepare("SELECT COUNT(*) FROM cursos WHERE nombre_curso LIKE :search");
 $total_sql->bindValue(':search', "%$search%", PDO::PARAM_STR);
 $total_sql->execute();
 $total_cursos = $total_sql->fetchColumn();
@@ -26,12 +26,12 @@ $total_pages = ceil($total_cursos / $items_per_page);
         <div class="col">
             <div class="card h-100 d-flex flex-column">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $curso["nombre"]?></h5>
-                    <p class="card-text"><?php echo $curso["descripcion"]?></p>
+                    <h5 class="card-title"><?php echo $curso["nombre_curso"]?></h5>
+                    <p class="card-text"><?php echo $curso["descripcion_curso"]?></p>
                 </div>
                 <div class="card-footer bg-transparent border-0 mt-auto">
                     <div class="text-center">
-                        <a href="curso.php?id_curso=<?php echo $curso["id"]?>" class="btn btn-primary">Más Información</a>
+                        <a href="curso.php?id_curso=<?php echo $curso["id_curso"]?>" class="btn btn-primary">Más Información</a>
                     </div>
                 </div>
             </div>
