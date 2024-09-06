981b02811ee99fb8afac3b547e02ff7cc63a1ca4
Ju4nix159 <juanimelillo@gmail.com>
2024-09-06 05:25:12 -0300

    cambio del nav dependiendo la sesion iniciada

--- nav.php
+++ nav.php
@@ -1,3 +1,7 @@
+<?php
+session_start()
+?>
+
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -6,36 +10,50 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-secondary sticky-top">
     <div class="container">
         <a class="navbar-brand" href="index.php">
-        <img src="logos/LOGO PNG-10.png" alt="Logo">
+            <img src="logos/LOGO PNG-10.png" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="index.php#quienes-somos">Nosotros</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="index.php#servicios-capacitacion">Servicios</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="index.php#cursos">Cursos</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="index.php#contacto">Contáctanos</a>
-            </li>
-            <li class="nav-item">
-                <a class="text-decoration-none" href="login.php">
-                    <button class="button-nav">
-                        iniciar sesion
-                        <div class="arrow-wrapper">
-                            <div class="arrow"></div>
-                        </div>
-                    </button>
-                </a>
-            </li>
-        </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#quienes-somos">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#servicios-capacitacion">Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#cursos">Cursos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php#contacto">Contáctanos</a>
+                </li>
+                <?php
+                if (isset($_SESSION["id_usuario"])) { ?>
+                    <li class="nav-item">
+                        <a class="text-decoration-none" href="admin/admin.php">
+                            <button class="button-nav">
+                                panel adm
+                                <div class="arrow-wrapper">
+                                    <div class="arrow"></div>
+                                </div>
+                            </button>
+                        </a>
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="text-decoration-none" href="login.php">
+                            <button class="button-nav">
+                                iniciar sesion
+                                <div class="arrow-wrapper">
+                                    <div class="arrow"></div>
+                                </div>
+                            </button>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
         </div>
     </div>
 </nav>
