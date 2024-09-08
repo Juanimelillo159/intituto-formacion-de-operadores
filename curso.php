bcfca7a0db030b1fbf2ada66668238ca21385440
Ju4nix159 <juanimelillo@gmail.com>
2024-09-07 22:36:12 -0300

    cambios en las paginas de los cursos
    Se cambio el texto a blanco para mejora el contraste con el fondo

--- curso.php
+++ curso.php
@@ -1,72 +1,62 @@
-<?php 
-    require_once 'sbd.php';
-    include ("nav.php");
-    
-    $id_curso = $_GET['id_curso'];
+<?php
+require_once 'sbd.php';
+include("nav.php");
+
+$id_curso = $_GET['id_curso'];
 
 
-    $sql_cursos = $con->prepare("SELECT * FROM cursos c JOIN complejidad n ON c.id_complejidad = n.id_complejidad WHERE c.id_curso = :id_curso");
-    $sql_cursos->bindParam(':id_curso', $id_curso);
-    $sql_cursos->execute();
-    $curso = $sql_cursos->fetch(PDO::FETCH_ASSOC);
-
-    
-
-    $sql_complejidad = $con->prepare("SELECT m.id_modalidad AS modalidad_id, m.nombre_modalidad AS modalidad_nombre FROM curso_modalidad cm JOIN modalidades m ON cm.id_modalidad = m.id_modalidad WHERE cm.id_curso = :id_curso");
-    $sql_complejidad->bindParam(':id_curso', $id_curso);
-    $sql_complejidad->execute();
-    $modalidades = $sql_complejidad->fetchAll(PDO::FETCH_ASSOC);
-
-    $modalidad_nombres = array();
-
-    foreach ($modalidades as $value) {
-        // Accede al nombre de la modalidad y lo agrega al array
-        $modalidad_nombres[] = htmlspecialchars($value['modalidad_nombre']);
-    }
-    $modalidad_nombres_str = implode(' - ', $modalidad_nombres);
+$sql_cursos = $con->prepare("SELECT * FROM cursos c JOIN complejidad n ON c.id_complejidad = n.id_complejidad WHERE c.id_curso = :id_curso");
+$sql_cursos->bindParam(':id_curso', $id_curso);
+$sql_cursos->execute();
+$curso = $sql_cursos->fetch(PDO::FETCH_ASSOC);
 
 
 
+$sql_complejidad = $con->prepare("SELECT m.id_modalidad AS modalidad_id, m.nombre_modalidad AS modalidad_nombre FROM curso_modalidad cm JOIN modalidades m ON cm.id_modalidad = m.id_modalidad WHERE cm.id_curso = :id_curso");
+$sql_complejidad->bindParam(':id_curso', $id_curso);
+$sql_complejidad->execute();
+$modalidades = $sql_complejidad->fetchAll(PDO::FETCH_ASSOC);
 
-    /* foreach ($modalidades as $key => $value) {
-        echo "<p><strong>$key:</strong> $value</p>";
-    } imprime todas los valores con clave valor de lo que devuelve una query  */
+$modalidad_nombres = array();
 
-   
+foreach ($modalidades as $value) {
+    // Accede al nombre de la modalidad y lo agrega al array
+    $modalidad_nombres[] = htmlspecialchars($value['modalidad_nombre']);
+}
+$modalidad_nombres_str = implode(' - ', $modalidad_nombres);
 
-    
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title><?php echo $curso["nombre_curso"]?></title>
+    <title><?php echo $curso["nombre_curso"] ?></title>
 </head>
+
 <body>
-<div class="jumbotron jumbotron-fluid py-5">
+    <div class="jumbotron jumbotron-fluid py-5 text-white">
         <div class="container">
-            <h1 class="display-4"><?php echo $curso["nombre_curso"]?></h1>
-            <p class="lead">subtitulo o explicacion rapida del curso</p>
+            <h1 class="display-4"><?php echo $curso["nombre_curso"] ?></h1>
+            <p class="lead ">subtitulo o explicacion rapida del curso</p>
         </div>
     </div>
 
     <div class="container my-5">
         <div class="row">
-            <div class="col-md-8">
-                <h2>Descripción del Curso</h2>
-                <p><?php echo $curso['descripcion_curso']?></p>
-                
+            <div class="col-md-8 text-white">
+                <h2 class="display-2 ">Descripción del Curso</h2>
+                <p><?php echo $curso['descripcion_curso'] ?></p>
+
                 <h3 class="mt-4">Objetivos del Curso</h3>
-                <ul>
-                    <li>objetivo 1</li>
-                    <li>objetivo 2</li>
-                    <li>objetivo 3</li>
-                    <li>objetivo 4</li>
-                </ul>
+                <P>
+                    <?php echo $curso['objetivos'] ?>
+                </P>
             </div>
             <div class="col-md-4">
                 <div class="card">
@@ -74,8 +64,8 @@
                         <h3 class="card-title">Detalles del Curso</h3>
                     </div>
                     <div class="card-body">
-                        <p><strong>Duración:</strong> <?php echo $curso["duracion"]?></p>
-                        <p><strong>Nivel:</strong> <?php echo $curso["nombre_complejidad"]?></p>
+                        <p><strong>Duración:</strong> <?php echo $curso["duracion"] ?></p>
+                        <p><strong>Nivel:</strong> <?php echo $curso["nombre_complejidad"] ?></p>
                         <p><strong>Modalidad:</strong> <?php echo $modalidad_nombres_str ?></p>
                         <p><strong>Certificación:</strong> Sí</p>
                         <a href="#" class="btn btn-primary btn-block">Inscribirse Ahora</a>
@@ -84,8 +74,9 @@
             </div>
         </div>
     </div>
-    <?php include("footer.php")?>
+    <?php include("footer.php") ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="app.js"></script>
 </body>
+
 </html>
